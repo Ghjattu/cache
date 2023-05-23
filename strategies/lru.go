8eb200515dcb1db09b531bc6c5f15961446d6eaf
ghjattu <icracked369@gmail.com>
2023-05-23 12:50:59 +0800

    lru

---
+++ strategies/lru.go
@@ -0,0 +1,125 @@
+package strategies
+
+import (
+	"container/list"
+)
+
+type Value interface {
+	Len() int
+}
+
+type entry struct {
+	key   string
+	value Value
+}
+
+// Cache is a LRU cache. It is not safe for concurrent access.
+type Cache struct {
+	// maximum memory can be used
+	maxBytes int64
+	// memory used currently
+	nBytes int64
+
+	ll    *list.List
+	cache map[string]*list.Element
+
+	// optional callback function and executed when an entry is purged from the cache
+	OnEvicted func(key string, value Value)
+}
+
+// New creates a new Cache
+func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		nBytes:    0,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: onEvicted,
+	}
+}
+
+// Add adds a value to the cache
+func (c *Cache) Add(key string, value Value) {
+	if c.cache == nil {
+		c.cache = make(map[string]*list.Element)
+		c.ll = list.New()
+	}
+
+	// key already exists, update value
+	if element, hit := c.cache[key]; hit {
+		c.ll.MoveToFront(element)
+		kv := element.Value.(*entry)
+		c.nBytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+
+		return
+	}
+
+	// key does not exist, add an item
+	element := c.ll.PushFront(&entry{key, value})
+	c.cache[key] = element
+	c.nBytes += int64(len(key)) + int64(value.Len())
+
+	if c.maxBytes != 0 && c.maxBytes < c.nBytes {
+		c.RemoveOldest()
+	}
+}
+
+// Get looks up a key's value from the cache
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if c.cache == nil {
+		return nil, false
+	}
+
+	if element, hit := c.cache[key]; hit {
+		c.ll.MoveToFront(element)
+		kv := element.Value.(*entry)
+
+		return kv.value, true
+	}
+
+	return nil, false
+}
+
+// Remove removes the provided key from the cache
+func (c *Cache) Remove(key string) {
+	if c.cache == nil {
+		return
+	}
+
+	if element, hit := c.cache[key]; hit {
+		c.removeElement(element)
+	}
+}
+
+// RemoveOldest removes the oldest item from cache
+func (c *Cache) RemoveOldest() {
+	if c.cache == nil {
+		return
+	}
+
+	element := c.ll.Back()
+	if element != nil {
+		c.removeElement(element)
+	}
+}
+
+func (c *Cache) removeElement(e *list.Element) {
+	c.ll.Remove(e)
+	kv := e.Value.(*entry)
+	delete(c.cache, kv.key)
+	c.nBytes -= int64(len(kv.key)) + int64(kv.value.Len())
+
+	if c.OnEvicted != nil {
+		c.OnEvicted(kv.key, kv.value)
+	}
+}
+
+// Len returns the number of items in the cache
+func (c *Cache) Len() int {
+	if c.cache == nil {
+		return 0
+	}
+
+	return c.ll.Len()
+}
---
+++ strategies/lru_test.go
@@ -0,0 +1,92 @@
+package strategies
+
+import (
+	"reflect"
+	"testing"
+)
+
+type String string
+
+func (s String) Len() int {
+	return len(s)
+}
+
+func TestAdd(t *testing.T) {
+	var lru Cache
+	beforeLen := lru.Len()
+	lru.Add("key1", String("value1"))
+	afterLen := lru.Len()
+	if afterLen != beforeLen+1 {
+		t.Error("add key1=value1 failed")
+	}
+
+	lru.Add("key1", String("v1"))
+	afterLen = lru.Len()
+	if v, ok := lru.Get("key1"); !ok || afterLen != beforeLen+1 || string(v.(String)) != "v1" {
+		t.Error("key exists, update failed")
+	}
+}
+
+func TestGet(t *testing.T) {
+	lru := New(0, nil)
+	lru.Add("key1", String("value1"))
+	if v, ok := lru.Get("key1"); !ok || string(v.(String)) != "value1" {
+		t.Error("hit key1=value1 failed")
+	}
+	if _, ok := lru.Get("key2"); ok {
+		t.Error("miss key2 failed")
+	}
+}
+
+func TestRemoveOldest(t *testing.T) {
+	k1, k2, k3 := "k1", "k2", "k3"
+	v1, v2, v3 := "v1", "v2", "v3"
+	lru := New(int64(len(k1+k2+v1+v2)), nil)
+	lru.Add(k1, String(v1))
+	lru.Add(k2, String(v2))
+	lru.Add(k3, String(v3))
+
+	if _, ok := lru.Get("k1"); ok || lru.Len() != 2 {
+		t.Error("remove oldest k1 failed")
+	}
+
+	if v, ok := lru.Get("k3"); !ok || string(v.(String)) != "v3" {
+		t.Error("hit k3=v3 failed")
+	}
+}
+
+func TestRemove(t *testing.T) {
+	k1, k2, k3 := "k1", "k2", "k3"
+	v1, v2, v3 := "v1", "v2", "v3"
+	lru := New(int64(len(k1+k2+k3+v1+v2+v3)), nil)
+	lru.Add(k1, String(v1))
+	lru.Add(k2, String(v2))
+	lru.Add(k3, String(v3))
+
+	lru.Remove("k2")
+	if _, ok := lru.Get("k2"); ok || lru.Len() != 2 {
+		t.Error("remove k2 failed")
+	}
+
+	if v, ok := lru.Get("k3"); !ok || string(v.(String)) != "v3" {
+		t.Error("hit k3=v3 failed")
+	}
+}
+
+func TestOnEvicted(t *testing.T) {
+	keys := make([]string, 0)
+	callback := func(key string, value Value) {
+		keys = append(keys, key)
+	}
+
+	lru := New(int64(10), callback)
+	lru.Add("key1", String("123456"))
+	lru.Add("k2", String("v2"))
+	lru.Add("k3", String("v3"))
+	lru.Add("k4", String("v4"))
+
+	expected := []string{"key1", "k2"}
+	if !reflect.DeepEqual(expected, keys) {
+		t.Errorf("call onEvicted failed, expected: %v, but %v\n", expected, keys)
+	}
+}
